4e417726afd57b4303238ec18bd57c80c877b88f
Amadou Moctar Diallo <mctrjalloh@gmail.com>
2018-12-15 05:58:20 +0000

    Updates

---
+++ precache-manifest.d35607e2ad657553097249276e809f90.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0fddcd108bd17086e708c8bd5d84eb0",
+    "url": "/moctardiallo/seneweb/static/media/senefoot.b0fddcd1.jpg"
+  },
+  {
+    "revision": "dff665e5a0a2a0df72e83427d883a736",
+    "url": "/moctardiallo/seneweb/static/media/publicite.dff665e5.gif"
+  },
+  {
+    "revision": "2bc95b301c0db6feb5d7b72706d10f50",
+    "url": "/moctardiallo/seneweb/static/media/mctr.2bc95b30.jpg"
+  },
+  {
+    "revision": "56c1cc2e379e8d0c5dd150deeada24ce",
+    "url": "/moctardiallo/seneweb/static/media/mane.56c1cc2e.jpg"
+  },
+  {
+    "revision": "786701999cfd6c484d6ae157613af880",
+    "url": "/moctardiallo/seneweb/static/media/cetud.78670199.jpg"
+  },
+  {
+    "revision": "920c67355a591fac34ac",
+    "url": "/moctardiallo/seneweb/static/js/runtime~main.920c6735.js"
+  },
+  {
+    "revision": "8a491de875a7cbb5d1a2",
+    "url": "/moctardiallo/seneweb/static/js/main.8a491de8.chunk.js"
+  },
+  {
+    "revision": "cf03ae66875f149f739f",
+    "url": "/moctardiallo/seneweb/static/js/1.cf03ae66.chunk.js"
+  },
+  {
+    "revision": "8a491de875a7cbb5d1a2",
+    "url": "/moctardiallo/seneweb/static/css/main.71aa12c6.chunk.css"
+  },
+  {
+    "revision": "eab3e2b741b29950e97673b1bbee0626",
+    "url": "/moctardiallo/seneweb/index.html"
+  }
+];
